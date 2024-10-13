1e62ee72934adf45b4dd22e70b004780d8710b61
jible <jamezman88th@gmail.com>
2024-10-13 16:30:30 -0700

    Step 1 Cleaned up

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ app.innerHTML = APP_NAME;
 
 // Create canvas
 const canvas = document.createElement("canvas");
-canvas.className = "canvas";
+canvas.className = "kaku-canvas";
 canvas.width = 256;
 canvas.height = 256;
 app.appendChild(canvas);
