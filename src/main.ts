3bf38926228c0fb325a47e3fdff68f0ce32d207d
jible <jamezman88th@gmail.com>
2024-10-15 13:07:19 -0700

    I got excited about the undo function and started working on that but ill do step 3 now

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,36 @@ canvas.width = 256;
 canvas.height = 256;
 app.appendChild(canvas);
 
+interface point{
+    x: number;
+    y: number;
+}
+interface line{
+    start: point;
+    end: point;
+}
+
+
+let createdLines:Array<line> = new Array<line>;
+let undoneLines:Array<line> = new Array<line>;
+function drawLine ( newLine: line){
+    createdLines.push(newLine);
+}
+function undo(): void{
+    const undoneLine:line|undefined  = createdLines.pop()
+    if (undoneLine){
+        undoneLines.push(undoneLine)
+    }
+    
+}
+function redo(): void{
+    const redoneLine: line|undefined = undoneLines.pop();
+    if (redoneLine){
+        createdLines.push(redoneLine)
+    }
+}
+
+
 
 addEventListener("mousemove", (event)=>{
     event
@@ -32,16 +62,20 @@ canvas.addEventListener("mouseup", () => {
     cursor.active = false;
 });
 
-  canvas.addEventListener("mousemove", (e) => {
+canvas.addEventListener("mousemove", (e) => {
     if (cursor.active && ctx) {
-      ctx.beginPath();
-      ctx.moveTo(cursor.x, cursor.y);
-      ctx.lineTo(e.offsetX, e.offsetY);
-      ctx.stroke();
-      cursor.x = e.offsetX;
-      cursor.y = e.offsetY;
+        
+
+
+        ctx.beginPath();
+        ctx.moveTo(cursor.x, cursor.y);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
     }
-  });
+});
+
 
 const clearButton = document.createElement("button");
 clearButton.innerHTML = "clear";
@@ -51,4 +85,12 @@ clearButton.addEventListener("click", () => {
     if (ctx){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
+});
+
+const undoButton = document.createElement("button");
+clearButton.innerHTML = "undo";
+document.body.append(clearButton);
+
+undoButton.addEventListener("click", () => {
+    undo();
 });
