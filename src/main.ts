f80038329e351c65397625da910bdd739e610f70
jible <jamezman88th@gmail.com>
2024-10-18 23:21:01 -0700

    added buttons for emojis- will give them functionality later

--- src/main.ts
+++ src/main.ts
@@ -12,45 +12,43 @@ canvas.className = "kaku-canvas";
 canvas.width = 256;
 canvas.height = 256;
 app.appendChild(canvas);
-
 // Events
 const canvasUpdate: Event = new Event("drawing-changed");
 const toolChanged: Event = new Event("tool-changed");
-
 // tool settings
 let currentColor: string = "#000000"; // Default color
 let currentThickness: number = 1;
 
-const cursor = {
-  active: false,
-  x: 0,
-  y: 0,
-};
-
+// --------------------------------------------------------------------------------------------------------
 // Imports
+// --------------------------------------------------------------------------------------------------------
 import { point, line, drag, addLine, drawDrag } from "./dataTypes.ts";
+import { wash } from "./drawCommands.ts";
 import {
   addColorPicker,
   addThicknessSlider,
   createButton,
 } from "./settingButtons.ts";
-import { wash } from "./drawCommands.ts";
-
+// --------------------------------------------------------------------------------------------------------
 // Setting up cursor and preview
-const toolPreview = {
-  active: false,
+// --------------------------------------------------------------------------------------------------------
+enum penModes {
+  PEN,
+  STAMP,
+}
+const pen = {
+  mode: penModes.PEN,
+  currentStamp: "🎲",
+  previewActive: false,
+  penDown: false,
   x: 0,
   y: 0,
-  updatePosition(x?: number, y?: number) {
-    if (x) {
-      this.x = x;
-    }
-    if (y) {
-      this.y = y;
-    }
+  updatePosition(x: number, y: number) {
+    this.x = x;
+    this.y = y;
   },
   draw(ctx: CanvasRenderingContext2D) {
-    if (this.active) {
+    if (this.previewActive) {
       ctx.fillStyle = currentColor;
       ctx.font = `32px monospace`;
       ctx.fillText("*", this.x - 8, this.y + 16);
@@ -58,8 +56,18 @@ const toolPreview = {
   },
 };
 
-// canvas data type and functions
-
+function drawCanvas(): void {
+  if (ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (const i of drags) {
+      drawDrag(i, ctx);
+    }
+    pen.draw(ctx);
+  }
+}
+// --------------------------------------------------------------------------------------------------------
+// Establishing arrays for storing lines
+// --------------------------------------------------------------------------------------------------------
 let drags: drag[] = [];
 let undoneDrags: drag[] = [];
 let currentDrag: drag = {
@@ -68,7 +76,67 @@ let currentDrag: drag = {
   color: currentColor,
 };
 const ctx = canvas.getContext("2d");
+// --------------------------------------------------------------------------------------------------------
+// Triggers for drawing and updating canvas
+// --------------------------------------------------------------------------------------------------------
+canvas.addEventListener("tool-changed", function () {
+  drawCanvas();
+});
 
+canvas.addEventListener("mouseout", (e) => {
+  pen.previewActive = false;
+  canvas.dispatchEvent(toolChanged);
+});
+
+canvas.addEventListener("mouseenter", (e) => {
+  pen.previewActive = true;
+  pen.updatePosition(e.offsetX, e.offsetY);
+  canvas.dispatchEvent(toolChanged);
+});
+
+canvas.addEventListener("mousedown", (e) => {
+  console.log(drags);
+  pen.penDown = true;
+  pen.x = e.offsetX;
+  pen.y = e.offsetY;
+  drags.push(currentDrag);
+});
+
+canvas.addEventListener("mouseup", () => {
+  pen.penDown = false;
+  if (currentDrag.lines.length == 0) {
+    drags.pop();
+  } else {
+    currentDrag = {
+      lines: [],
+      thickness: currentThickness,
+      color: currentColor,
+    };
+  }
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (pen.penDown && ctx) {
+    const start: point = { x: pen.x, y: pen.y };
+    const end: point = { x: e.offsetX, y: e.offsetY };
+    const newLine: line = { start, end };
+    addLine(currentDrag, newLine);
+    pen.x = e.offsetX;
+    pen.y = e.offsetY;
+    undoneDrags.length = 0;
+    canvas.dispatchEvent(canvasUpdate);
+  }
+  pen.updatePosition(e.offsetX, e.offsetY);
+  canvas.dispatchEvent(toolChanged);
+});
+
+canvas.addEventListener("drawing-changed", function () {
+  drawCanvas();
+});
+
+// --------------------------------------------------------------------------------------------------------
+// Buttons and associated functions
+// --------------------------------------------------------------------------------------------------------
 function clear(): void {
   currentDrag = {
     lines: [],
@@ -81,7 +149,6 @@ function clear(): void {
   drags = [];
   undoneDrags = [];
 }
-
 function undo(): void {
   const undoneDrag: drag | undefined = drags.pop();
   if (undoneDrag) {
@@ -96,76 +163,17 @@ function redo(): void {
   }
   canvas.dispatchEvent(canvasUpdate);
 }
-function drawCanvas(): void {
-  if (ctx) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (const i of drags) {
-      drawDrag(i, ctx);
-    }
-    toolPreview.draw(ctx);
-  }
-}
-
-// Triggers for drawing and updating canvas
-canvas.addEventListener("tool-changed", function () {
-  drawCanvas();
+const _clearButton = createButton("clear", app, () => {
+  clear();
+});
+const _undoButton = createButton("undo", app, () => {
+  undo();
+});
+const _redoButton = createButton("redo", app, () => {
+  redo();
 });
 
-canvas.addEventListener("mouseout", (e) => {
-  toolPreview.active = false;
-  canvas.dispatchEvent(toolChanged);
-});
-
-canvas.addEventListener("mouseenter", (e) => {
-  toolPreview.active = true;
-  toolPreview.updatePosition(e.offsetX, e.offsetY);
-  canvas.dispatchEvent(toolChanged);
-});
-
-canvas.addEventListener("mousedown", (e) => {
-  console.log(drags);
-  cursor.active = true;
-  cursor.x = e.offsetX;
-  cursor.y = e.offsetY;
-  drags.push(currentDrag);
-});
-
-canvas.addEventListener("mouseup", () => {
-  cursor.active = false;
-  if (currentDrag.lines.length == 0) {
-    drags.pop();
-  } else {
-    currentDrag = {
-      lines: [],
-      thickness: currentThickness,
-      color: currentColor,
-    };
-  }
-});
-
-canvas.addEventListener("mousemove", (e) => {
-  if (cursor.active && ctx) {
-    const start: point = { x: cursor.x, y: cursor.y };
-    const end: point = { x: e.offsetX, y: e.offsetY };
-    const newLine: line = { start, end };
-    addLine(currentDrag, newLine);
-    cursor.x = e.offsetX;
-    cursor.y = e.offsetY;
-    undoneDrags.length = 0;
-    canvas.dispatchEvent(canvasUpdate);
-  }
-  toolPreview.updatePosition(e.offsetX, e.offsetY);
-  canvas.dispatchEvent(toolChanged);
-});
-
-canvas.addEventListener("drawing-changed", function () {
-  drawCanvas();
-});
-
-// Buttons
 const colorPicker = addColorPicker(app);
-// Handle color changes
-
 colorPicker.addEventListener("input", (event) => {
   const target = event.target as HTMLInputElement;
   currentColor = target.value;
@@ -173,6 +181,7 @@ colorPicker.addEventListener("input", (event) => {
   canvas.dispatchEvent(toolChanged);
   // Update line color or apply it wherever necessary
 });
+
 const thicknessSlider = addThicknessSlider(app);
 thicknessSlider.addEventListener("input", (event) => {
   const target = event.target as HTMLInputElement;
@@ -182,12 +191,19 @@ thicknessSlider.addEventListener("input", (event) => {
   canvas.dispatchEvent(toolChanged);
 });
 
-const _clearButton = createButton("clear", app, () => {
+// emoji buttons
+const startingEmojis: string[] = ["😂", "🚀", "🎲"];
+const emojiButtons: HTMLElement[] = [];
+for (const emoji of startingEmojis) {
+  emojiButtons.push(
+    createButton(emoji, app, () => {
+      // set to emoji mode- maybe send signal?
+      // set emoji drawing tool to use current emoji
+      pen.mode = penModes.STAMP;
+      pen.currentStamp = emoji;
+    })
+  );
+}
+const _ = createButton("clear", app, () => {
   clear();
-});
-const _undoButton = createButton("undo", app, () => {
-  undo();
-});
-const _redoButton = createButton("redo", app, () => {
-  redo();
 });
